9de01cc3153cb4d987f5b24eb94582cfcb3c9f61
roger <rogergutierrezmartinez900@gmail.com>
2025-12-29 12:32:22 -0500

    se ajusta diseño en resolución mobile

--- src/public/pages/HomePage/HomePage.tsx
+++ src/public/pages/HomePage/HomePage.tsx
@@ -3,10 +3,10 @@ const HomePage = () => {
     <section
       className={`home-container flex  justify-center items-center bg-[url('src/assets/home/background-home-desktop.jpg')] bg-cover bg-center bg-no-repeat pt-28 w-full h-full`}
     >
-      <article className="flex flex-col items-center justify-center gap-10 pb-10 sm:flex sm:flex-row sm:justify-between sm:items-center sm:w-full sm:px-24">
-        <article className="flex flex-col justify-center items-start gap-10 w-[300px] max-w-[80%]">
-          <h1 className="w-full">
-            <p className="text-xl">So, you want to travel to</p>
+      <article className="flex flex-col items-center justify-center gap-10 w-full px-12 pb-10 sm:flex sm:flex-row sm:justify-between sm:items-center sm:w-full sm:px-24">
+        <article className="title-container flex flex-col items-center justify-center gap-10 sm:flex sm:flex-col sm:justify-center sm:items-start sm:gap-10 sm:w-[300px] sm:max-w-[80%]">
+          <h1 className="sm:w-full">
+            <p className="text-xl mb-4">So, you want to travel to</p>
             <p className="text-7xl font-normal tracking-[0.6rem]">Space</p>
           </h1>
           <p className="text-sm text-justify">
