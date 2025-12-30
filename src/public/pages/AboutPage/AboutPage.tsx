6f049cb27a6635b408bcafbb80f35c100b281e2d
roger <rogergutierrezmartinez900@gmail.com>
2025-12-30 00:17:04 -0500

    eliminacion de archivo innecesario

--- src/public/pages/AboutPage/AboutPage.tsx
+++
@@ -1,8 +0,0 @@
-const AboutPage = () => {
-  return (
-    <div className="about-page-container flex justify-center items-center pt-20 bg-green-900 w-full h-full">
-      AboutPage
-    </div>
-  );
-};
-export default AboutPage;
