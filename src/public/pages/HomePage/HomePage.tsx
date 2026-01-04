cbd48b7a809f692373ca78ea5d77115522630516
roger2999 <rogergutierrezmartinez900@gmail.com>
2026-01-04 02:02:41 -0500

    Refactor styles for HomePage and remove unused styles from DestinationPage

--- src/public/pages/HomePage/HomePage.tsx
+++ src/public/pages/HomePage/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
     <section
       className={`home-page-container flex  justify-center items-center bg-[url('src/assets/home/background-home-desktop.jpg')] bg-cover bg-center bg-no-repeat pt-28 w-full h-full`}
     >
-      <article className="content flex flex-col items-center justify-center gap-10 w-full px-12 pb-10 sm:mt-20 sm:flex sm:flex-row sm:justify-between sm:items-center sm:w-full sm:px-5 lg:px-32">
+      <article className="content flex flex-col items-center justify-center gap-10 w-full px-12 pb-10 sm:mt-20 sm:flex sm:flex-row sm:justify-between sm:items-center sm:w-full sm:px-5 sm:pr-20 lg:px-32">
         <article className="title-container flex flex-1 flex-col items-center justify-center gap-10 sm:flex sm:flex-col sm:justify-center sm:items-start sm:gap-0 sm:w-[500px] sm:max-w-[80%]">
           <h1 className="title sm:w-full">
             <span className="text-[32px] mb-4">So, you want to travel to</span>
