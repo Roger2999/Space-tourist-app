e1cfbd81ad8bbf655c23a783ca4e26c144e44227
roger2999 <rogergutierrezmartinez900@gmail.com>
2026-01-04 21:01:32 -0500

    Remove external font links from index.html and add @fontsource packages for Barlow and Bellefair fonts in package.json and package-lock.json. Update main.tsx to import new fonts and adjust TechnologyPage layout for better order of elements.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/_variables.css";
 import "./styles/global.css";
+import "@fontsource/bellefair";
+import "@fontsource/barlow-condensed";
+import "@fontsource/barlow";
 import { SpaceRoutes } from "./SpaceRoutes";
 
 createRoot(document.getElementById("root")!).render(
--- src/public/pages/TechnologyPage/TechnologyPage.tsx
+++ src/public/pages/TechnologyPage/TechnologyPage.tsx
@@ -32,7 +32,7 @@ const TechnologyPage = () => {
   return (
     <section className="technology-page-container flex flex-col justify-center bg-cover bg-center bg-no-repeat pt-28 w-full h-full">
       <section className="content flex flex-col w-full h-full gap-20 px-10 pb-10 sm:pb-0 sm:px-14 md:px-20 md:pb-0 md:pr-0 md:flex-row md:gap-10">
-        <article className="flex flex-1 flex-col justify-start gap-10 sm:gap-28">
+        <article className="flex flex-1 flex-col justify-start gap-10 sm:gap-28 order-2 sm:order-2 md:order-1">
           <h1 className="title text-xl my-7">03 SPACE LAUNCH 101</h1>
           <article className="flex flex-col gap-5">
             <div className="flex gap-10 max-w-full min-h-72 md:gap-14 ">
@@ -67,7 +67,7 @@ const TechnologyPage = () => {
             </div>
           </article>
         </article>
-        <aside className="flex-1 flex justify-center items-center md:justify-end">
+        <aside className="flex-1 flex justify-center items-center md:justify-end order-1 sm:order-1 md:order-2">
           <figure>
             <img
               src={selectedTechnology?.images.portrait}
